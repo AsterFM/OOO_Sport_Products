99e6b03eea8969e204b5d6dee5c11c618ca13d04
Александр Кувшинов <asterfm2000@gmail.com>
2023-11-18 15:02:12 +0300

    FiltrGotov

--- OOO_Sport_Products/OOO_Sport_Products/View/WindowsCatalog.xaml.cs
+++ OOO_Sport_Products/OOO_Sport_Products/View/WindowsCatalog.xaml.cs
@@ -2,6 +2,8 @@
 using OOO_Sport_Products.Model;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity.Infrastructure.Interception;
+using System.Data.SqlTypes;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -53,22 +55,108 @@ namespace OOO_Sport_Products.View
             cbCategory.DisplayMemberPath = "CategoryName";
             cbCategory.SelectedValuePath = "CategoryId";
             cbCategory.SelectedIndex = 0;
-
+            rbSortAsc.IsChecked = true;
+            ShowProducts();
             //
+            //    List<Model.Product> listProducts = new List<Model.Product>();
+            //    listProducts = Helper.DB.Products.ToList();
+            //    //listBoxProducts.ItemsSource = listProducts;  
+            //    List<Classes.ProductExtended> productExtendeds = new List<Classes.ProductExtended>();
+            //    foreach (Model.Product product in listProducts) 
+            //    {
+            //        Classes.ProductExtended productExtended = new Classes.ProductExtended();
+            //        productExtended.Product = product;
+            //        productExtendeds.Add(productExtended);
+            //    }
+
+            //    listBoxProducts.ItemsSource = productExtendeds;
+            //}
+
+
+        }
+        //Отображает инофрмацию обо всех товарах с фильтрацией
+        private void ShowProducts()
+        {
             List<Model.Product> listProducts = new List<Model.Product>();
             listProducts = Helper.DB.Products.ToList();
             //listBoxProducts.ItemsSource = listProducts;  
             List<Classes.ProductExtended> productExtendeds = new List<Classes.ProductExtended>();
-            foreach (Model.Product product in listProducts) 
+            int totalCount = listProducts.Count;
+            foreach (Model.Product product in listProducts)
             {
                 Classes.ProductExtended productExtended = new Classes.ProductExtended();
                 productExtended.Product = product;
                 productExtendeds.Add(productExtended);
             }
-
+            //Сортировка
+            if (rbSortAsc.IsChecked == true)
+            {
+                productExtendeds = productExtendeds.OrderBy(x => x.Product.ProductCost).ToList();
+            }
+            else 
+            {
+                productExtendeds = productExtendeds.OrderByDescending(x => x.Product.ProductCost).ToList();
+            }
+            double min = 0; //Все диапазоны скидок
+            double max = 100;
+            //Выбор диапазона скидки
+            switch (cbDiscount.SelectedIndex) 
+            {
+                case 1:
+                    min = 0;
+                    max = 9.99;
+                    break;
+                case 2:
+                    min = 10;
+                    max = 14.99;
+                    break;
+                case 3:
+                    min = 15;
+                    max = 100;
+                    break;
+            }
+            //Сортировка по скидке
+            productExtendeds = productExtendeds.Where(x => x.Product.ProductDiscountMax >= min && x.Product.ProductDiscountMax <= max).ToList();
+            if (cbCategory.SelectedValue != null) 
+            {
+                int idCat = (int)cbCategory.SelectedValue;
+                if (idCat > 0) 
+                {
+                    productExtendeds = productExtendeds.Where(x => x.Product.ProductCutegoryId == idCat).ToList();
+                }
+            }
+            //Поиск по названию
+            string searchName = tbSearch.Text.ToUpper();
+            if (!string.IsNullOrEmpty(searchName))
+                productExtendeds = productExtendeds.Where(x => x.Product.ProductName.ToUpper().Contains(searchName)).ToList();
+            int filterCount = productExtendeds.Count;
+            tbCount.Text = filterCount.ToString() + " из " + totalCount.ToString();
             listBoxProducts.ItemsSource = productExtendeds;
         }
-
-
+        //Сортировка по возростанию
+        private void rbSortAsc_Checked(object sender, RoutedEventArgs e)
+        {
+            ShowProducts();
+        }
+        //Сортировка по убыванию
+        private void rbSortDesc_Checked(object sender, RoutedEventArgs e)
+        {
+            ShowProducts();
+        }
+        //Сотрировка по скидке
+        private void cbDiscount_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ShowProducts();
+        }
+        //Фильтрация по категории
+        private void cbCategory_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ShowProducts();
+        }
+        //Ввод в строку поиска
+        private void tbSearch_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            ShowProducts();
+        }
     }
 }
